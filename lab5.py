1f6cf964735ba88c2a318c1ebcf21616b195e468
bakdauletov <bakdauletovv@yandex.ru>
2023-10-12 03:15:50 +0600

    Lab 3

---
+++ lab5.py
@@ -0,0 +1,118 @@
+# digit= int(input("Number your: "))
+# digit = int(digit)
+# thousands = digit // 1000
+# handreds = (digit // 100) % 10
+# tenz = (digit // 10) % 10
+# one = digit % 10
+# if thousands + one == abs(handreds - tenz):
+#   print("Yes")
+# else:
+#   print("No")
+
+# number = int(input("eNTER"))
+# if number < 18:
+#   print("Access denied")
+# else:
+#   print("Access permitted")
+
+# number = int(input("First "))
+# number2 = int(input("Second "))
+# number3 = int(input("Third "))
+# if number2 == number + 1 and number3 == number2 + 1:
+#     print("Yes")
+# else:
+#     print("No")
+
+# x1 = int(input())
+# y1 = int(input())
+# x2 = int(input())
+# y2 = int(input())
+# if x1 == x2 or y1 == y2:
+#     print("YES")
+# else:
+#     print("NO")
+
+# x1 = int(input("Enter the column number for the first cell (1-8): "))
+# y1 = int(input("Enter the row number for the first cell (1-8): "))
+# x2 = int(input("Enter the column number for the second cell (1-8): "))
+# y2 = int(input("Enter the row number for the second cell (1-8): "))
+
+# if abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1:
+#     print("YES")
+# else:
+#     print("NO")
+
+# number = int(input("First "))
+# number2 = int(input("First "))
+# number3 = int(input("Third "))
+# if number2 == number + 1 and number3 == number2 + 1:
+#     print(number2)
+# else:
+#     print((number + number2 + number3) - min(number, number2, number3) - max(number, number2, number3))
+
+# try:
+#     month = int(input("Enter: "))
+#     if month > 12:
+#         raise ValueError("You must enter number between 1 and 12!")
+#     if month == 2:
+#         print("28")
+#     if month in (9,6,4,11):
+#         print("30")
+#     else: 
+#         print("31")
+# except ValueError as ve:
+#     print(f"Error: {ve}")
+
+# number = int(input("Enter: "))
+# if number < 60:
+#     print("Light Weight")
+# elif 60 <= number < 64:
+#     print("First welterweight")
+# elif 64 <= number < 69:
+#     print("Welterweight")
+# else:
+#     print("Wrong weight")
+
+# password = input("Enter you password: ")
+# password2 = input("Enter your password again: ")
+# if password == password2:
+#     print("Correct one! Good job, nigga!")
+# else:
+#     print("Wrong! Try again, nigga!")
+
+# Number = int(input("Enter your number: "))
+# if (Number % 2) == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# number = int(input("Num1: "))
+# number1 = int(input("Num2: "))
+# if number > number1:
+#     print(number1)
+# elif number1 > number:
+#     print(number)
+# elif number1 == number:
+#     print(number)
+
+
+# age = int(input("Enter your age: "))
+# if age < 13:
+#     print("Childhood")
+# elif 14 < age < 24:
+#     print("Youth")
+# elif 25 < age < 59:
+#     print("Maturity")
+# else:
+#     print("Old")
+
+# Num = int(input("First: "))
+# Num2 = int(input("Second: "))
+# Num3 = int(input("Third: "))
+# if Num == Num2 == Num3:
+#     print("Equilateral")
+# elif Num == Num2 or Num == Num3 or Num2 == Num3:
+#     print("Isosceles")
+# else:
+#     print("Versatile")
+
